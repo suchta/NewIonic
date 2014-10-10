86ee77e949e63612925fa22e2a2c3e6c9d33e22d
Tutash <suchta@gmail.com>
2014-10-10 08:15:04 -0400

    Friend tab
    Added appropriately coded asynchronous call to an external API. Also
    did some fun binding.

--- myNewApp/www/js/controllers.js
+++ myNewApp/www/js/controllers.js
@@ -19,27 +19,65 @@ angular.module('starter.controllers', [])
         }
 })
 
-.controller('FriendsCtrl', function($scope, Friends) {
-        console.log(' friends ctrl loaded ')
+.controller('FriendsCtrl', function($scope, Friends, $http, $q) {
+    console.log(' friends ctrl loaded ')
+
+        $scope.init = function(){
+            $scope.getImages()
+                .then(function(res){
+                    //success
+                  //  console.log('Images: ', res)
+                    $scope.imageList = res.shots;
+                },function(status){
+                    //error
+                    console.log('Error: ', status)
+                    $scope.pageError = status;
+                })
+        }
+
+        $scope.getImages = function(){
+            var defer = $q.defer();
+
+            $http.jsonp('http://api.dribbble.com/shots/popular?callback=JSON_CALLBACK')
+                .success(function(res){
+                    defer.resolve(res)
+                })
+                .error(function(status, err){
+                    defer.reject(status)
+                })
+
+            return defer.promise;
+
+        }
+        $scope.init();
+
+
+
     $scope.friends = Friends.all();
 
 })
 
 .controller('FriendDetailCtrl', function($scope, $stateParams, Friends, $ionicLoading) {
   $scope.friend = Friends.get($stateParams.friendId);
+
+
+
 })
 
 
 
 .controller('AccountCtrl', function($scope, $ionicLoading) {
 
+        $scope.init = function(){
+
+        }
     console.log('loading account ctrl')
         $scope.long = '';
         $scope.lat = '...loading location';
         navigator.geolocation.getCurrentPosition(
 
             function(pos){
-                console.log('we have current position' + pos.coords.latitude)
+                /*console.log('we have current position' + pos.coords.latitude)*/
                 $scope.lat = Math.round(pos.coords.latitude * 1000)/1000;
                 $scope.long = Math.round(pos.coords.longitude * 1000)/1000;
                 $scope.$apply();
